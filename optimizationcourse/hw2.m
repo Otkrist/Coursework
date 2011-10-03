5afce62390910ef8ab780da058f91e77680a5a61
otkrist <otkrist@gmail.com>
2011-10-03 02:04:10 -0400

    function plots, minor changes

--- optimizationcourse/hw2.m
+++ optimizationcourse/hw2.m
@@ -1,5 +1,7 @@
 clear all
 close all
+
+%% Problem 1
 %c=[-2 -1 0 0]
 %b=[2 6]'
 %A = [[1 -1 1 0 ];
@@ -7,25 +9,22 @@ close all
 %]
 %simplesimplex_succint(c,A,b,[3 4])
 
-funct = {}
 
 
+%% Problem 2
 N = 100;
 a = -1;
 b = 1;
 r = linspace(a,b,N)';
 f1 = exp(r);
-
-figure;
-hold on;
-plot(r,f1,'b');
+soln_err = [];
+v={};
 
 % For each degree try out function %
 for deg=0:5,
 d = deg;
 
 [A b c] =getChebyShev(f1,d,r);
-size(A)
 
 % A x = b
 % minimize ( c . x )
@@ -34,20 +33,22 @@ N=size(A,2);
 [x,fval] = linprog(c,-eye(N,N),zeros(N,1),A,b);
 
 poly = zeros(d+1,1);
-polynomial_s = '0';
 for j=1:d+1
   poly(j) =  x(2*j-1) - x(2*j);
-  %polynomial_s = [polynomial_s '+%fx^' sprintf('%d',j-1) ];
 end
 
-%polynomial_s;
-%polynomial_s = sprintf(polynomial_s,poly)
-%f_sol = inline(polynomial_s)
-%funct{deg+1} = f_sol
-
-%h_func = @(x)(f_sol(x))
-%h_func= poly2sym(poly(end:-1:1));
-%v1 = arrayfun(h_func,r);
-v1 = polyval(poly(end:-1:1),r);
-plot(r,v1,'r');
+v{deg+1} = polyval(poly(end:-1:1),r);
+soln_err = [soln_err x(end-1)-x(end)];
 end
+
+figure;
+plot(r,f1,r,v{1},r,v{2},r,v{3},r,v{4},r,v{5},r,v{6})
+legend('e^x','deg 0','deg 1','deg 2','deg 3','deg 4','deg 5')
+title('Function plots')
+saveas(gcf,'Solutionplots.png')
+
+
+figure
+plot([0:5],soln_err);
+title('Solution Error');
+saveas(gcf,'SolutionError.png')
